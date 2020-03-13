51420ed6987660161e272ca6785b58331e89c5bf
Alena Belyavskaya <alena.velitskaya@gmail.com>
2020-03-13 16:30:27 +0400

    New project created

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook_tests_autoit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("addressbook_tests_autoit")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f906d10a-5eb4-4bd2-908b-3a45a0bf6e8a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ addressbook_tests_autoit/addressbook_tests_autoit/appmanager/GroupHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace addressbook_tests_autoit
+{
+    public class GroupHelper : HelperBase
+    {
+        public GroupHelper(ApplicationManager manager) : base(manager) { }
+
+        public List<GroupData> GetGroupsList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(GroupData newGroup)
+        {
+            
+        }
+    }
+}
